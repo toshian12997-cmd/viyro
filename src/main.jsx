75fb46032cd5009d01f2ca2760547b225955ca17
toshian12997-cmd <toshian12997@gmail.com>
2026-09-13 11:47:58 +0300

    Switch Vero to the refined production UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './styles.css';
+import VeroApp from './VeroApp.jsx';
+import './vero.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <VeroApp />
   </React.StrictMode>
 );
